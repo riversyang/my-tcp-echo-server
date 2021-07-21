85721efd05b18883a2d0a16408e01704e78c0259
Rivers Yang <rivers.yang@icloud.com>
2021-07-21 20:40:26 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::thread;
-use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
 use std::str;
+use std::thread;
 
 /**
  * Handle the input stream, keep running until error occurred
@@ -13,28 +13,40 @@ fn handle_client(mut stream: TcpStream) {
     while match stream.read(&mut data) {
         // successfully matched with the return value of function stream.read()
         Ok(size) => {
-            // convert u8 array to &str, remove the last character('\x0a')
-            let str = match str::from_utf8(&data[0..size - 1]) {
-                Ok(v) => v,
-                Err(_) => "Invalid UTF-8 sequence received."
-            };
-            // print received content
-            println!("Message received from {}: {}", stream.peer_addr().unwrap(), str);
-            // echo everything received
-            stream.write(&data[0..size]).unwrap();
-            // return true on success
-            true
-        },
+            if size > 0 {
+                // convert u8 array to &str, remove the last character('\x0a')
+                let str = match str::from_utf8(&data[0..size - 1]) {
+                    Ok(v) => v,
+                    Err(_) => "Invalid UTF-8 sequence received.",
+                };
+                // print received content
+                println!(
+                    "Message received from {}: {}",
+                    stream.peer_addr().unwrap(),
+                    str
+                );
+                // echo everything received
+                stream.write(&data[0..size]).unwrap();
+                // return true on success
+                true
+            } else {
+                false
+            }
+        }
         // handle error while reading data from input stream
         Err(_) => {
             // print exit message
-            println!("Error occurred while reading from input stream, terminating connection with {}", stream.peer_addr().unwrap());
+            println!(
+                "Error occurred while reading from input stream, terminating connection with {}",
+                stream.peer_addr().unwrap()
+            );
             // shutdown input stream
             stream.shutdown(Shutdown::Both).unwrap();
             // return false on error
             false
         }
     } {}
+    println!("Client from {} disconnected.", stream.peer_addr().unwrap());
 }
 
 fn main() {
@@ -49,7 +61,7 @@ fn main() {
                 // print client information
                 println!("New connection: {}", stream.peer_addr().unwrap());
                 // spawn a new thread for each client
-                thread::spawn(move|| {
+                thread::spawn(move || {
                     // actually handle the incoming stream
                     handle_client(stream)
                 });
